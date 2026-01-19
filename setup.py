ac0e90de8aaf557cc3f56cb83d593ed4200157d2
Shubham051299 <shubhamwagh0512@gmail.com>
2026-01-18 22:25:13 -0500

    requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot_with_llm',
+    version='0.1.0',
+    packages=find_packages(),
+    author="shubham wagh ",
+    author_email="shubhamwagh0512@gmail.com",
+    install_requires=[]
+)
