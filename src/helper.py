4ff3dc0ab8e555763cee73c1bfab752ca302f7f9
Shubham051299 <shubhamwagh0512@gmail.com>
2026-01-26 23:37:40 -0500

    app completed

---
+++ src/helper.py
@@ -0,0 +1,36 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+   
+
+from typing import List
+from langchain.schema import Document
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs : List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(Document(page_content=doc.page_content, metadata={"source": src}))
+    return minimal_docs
+
+#split documents into smaller chunks
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+        length_function=len,
+    )
+    text_chunks = text_splitter.split_documents(minimal_docs)
+    return text_chunks
+
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def download_embeddings():
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
+embeddings = download_embeddings()
---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+system_prompt = ("your are a helpful medical assistant."
+" Use the following context to answer the users question."
+" If you don't know the answer, just say that you don't know, don't try to make up an answer, "
+"{context}")
+
